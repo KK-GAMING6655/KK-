dcbc3cfadb7fc269fe9da6883075ba14b6685e3c
KK-GAMING6655 <karthikpb2009@gmail.com>
2026-03-14 09:24:01 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from aiohttp import web
 import discord
 from discord.ext import commands
 
-# ---------- Bot setup ----------
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
